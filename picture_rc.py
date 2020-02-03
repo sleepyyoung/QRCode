338fe67b6002b483b253e730a999952afbb55897
sleepyyoung <50011517+sleepyyoung@users.noreply.github.com>
2020-02-03 17:31:11 +0800

    界面图片/图标路径引入

--- picture_rc.py
+++ picture_rc.py
@@ -1,4 +1,5 @@
 '''
+
 文件太大，上传不上去
 
 这个文件与picture.qrc搭配，引入picture.qrc中的图片并将其转化为base64，以便在界面中 import picture_rc.py
@@ -6,5 +7,6 @@
 这样做就不用担心因本机图片路径改变导致图片在界面上显示不出来
 
 生成方式: pyrcc5 picture.qrc -o picture_rc.py
+
 '''
 
